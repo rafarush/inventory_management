6b5d43df949e3c681765c0e62708eabd9a6ae9e4
rafarush <rgarciagonzalez59@gmail.com>
2025-08-30 18:19:47 -0400

    chore: CSRF_TRUSTED_ORIGINS fixed

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -31,7 +31,7 @@ DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(',')
+CSRF_TRUSTED_ORIGINS = [os.getenv('CSRF_TRUSTED_ORIGINS')]
 
 # Application definition
 
