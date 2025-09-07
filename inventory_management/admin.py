693b6c84dddc66e473cd422c35a06369df9bec44
jQrgitQ <158132525+jQrgitQ@users.noreply.github.com>
2025-09-07 17:11:21 +0200

    feat: finished

--- inventory_management/admin.py
+++ inventory_management/admin.py
@@ -6,6 +6,7 @@ from inventory_management.modules.worker.admin import WorkerAdmin
 from inventory_management.modules.product.admin import ProductAdmin
 from inventory_management.modules.charge_cart.admin import ChargeCartAdmin
 from inventory_management.modules.daily_part_cart.admin import DailyPartCartAdmin
+from inventory_management.modules.store.admin import StoreAdmin
 from inventory_management.modules.product_category.admin import ProductCategoryAdmin
 from inventory_management.modules.custom_user.admin import CustomUserAdmin
 
