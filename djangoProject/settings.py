bfacd4aaa31bca6d1a36fc0334fd30221cb32342
rafarush <rgarciagonzalez59@gmail.com>
2025-09-16 09:49:15 -0400

    fix: partialy done

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -142,9 +142,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTH_USER_MODEL = 'inventory_management.CustomUser'
-LOGIN_URL = 'auth/login/'
+LOGIN_URL = '/auth/login/'
 LOGIN_REDIRECT_URL = ''
-SESSION_COOKIE_AGE = 600
+SESSION_COOKIE_AGE = 10
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
