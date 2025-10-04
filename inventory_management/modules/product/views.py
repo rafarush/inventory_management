cfef8e5732b17e819cc5611c83e86ab1ff6c26d7
jQrgitQ <158132525+jQrgitQ@users.noreply.github.com>
2025-10-05 01:47:27 +0200

    actions is missing

--- inventory_management/modules/product/views.py
+++ inventory_management/modules/product/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.template.loader import render_to_string
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DeleteView, DetailView, UpdateView
 from inventory_management.models import Product
@@ -14,7 +15,7 @@ class ProductListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     permission_required = 'inventory_management.view_product'
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data()
+        context = super().get_context_data(**kwargs)
         context['form'] = ProductAdminForm()
         return context
 
@@ -24,38 +25,43 @@ class ProductListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
         return super().handle_no_permission()
 
 
+# ------------------------- CREATE -------------------------
 class ProductCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Product
-    success_url = reverse_lazy('product_list')
-    template_name = 'product/product_form.html'
     form_class = ProductAdminForm
+    template_name = 'product/product_form.html'
+    success_url = reverse_lazy('product_list')
     permission_required = 'inventory_management.add_product'
 
+    def get(self, request, *args, **kwargs):
+        form = self.get_form()
+        context = {
+            'form': form,
+            'action_url': reverse_lazy('product_create')
+        }
+        if request.headers.get("x-requested-with") == "XMLHttpRequest":
+            html = render_to_string(self.template_name, context, request=request)
+            return JsonResponse({"success": True, "html": html})
+        return super().get(request, *args, **kwargs)
+
+    def form_invalid(self, form):
+        if self.request.headers.get("x-requested-with") == "XMLHttpRequest":
+            return JsonResponse({"errors": form.errors})
+        return super().form_invalid(form)
+
+    def form_valid(self, form):
+        form.save()
+        if self.request.headers.get("x-requested-with") == "XMLHttpRequest":
+            return JsonResponse({"success": True})
+        return super().form_valid(form)
+
     def handle_no_permission(self):
         if self.request.user.is_authenticated:
             return render(self.request, 'access_denied.html', status=403)
         return super().handle_no_permission()
 
 
-class ProductDetailView(LoginRequiredMixin, DetailView):
-    model = Product
-    context_object_name = 'product'
-    template_name = 'product/product_detail.html'
-
-
-class ProductDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
-    model = Product
-    template_name = 'product/product_confirm_delete.html'
-    success_url = reverse_lazy('product_list')
-    permission_required = 'inventory_management.delete_product'
-    context_object_name = 'product'
-
-    def handle_no_permission(self):
-        if self.request.user.is_authenticated:
-            return render(self.request, 'access_denied.html', status=403)
-        return super().handle_no_permission()
-
-
+# ------------------------- UPDATE -------------------------
 class ProductUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Product
     form_class = ProductUpdateForm
@@ -63,17 +69,81 @@ class ProductUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView)
     success_url = reverse_lazy('product_list')
     permission_required = 'inventory_management.change_product'
 
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        # Selección de formulario según grupo
+        if request.user.groups.filter(name='Administrators').exists():
+            self.form_class = ProductAdminForm
+
+        form = self.get_form()
+        context = {
+            'form': form,
+            'action_url': reverse_lazy('product_update', kwargs={'pk': self.object.pk})
+        }
+        if request.headers.get("x-requested-with") == "XMLHttpRequest":
+            html = render_to_string(self.template_name, context, request=request)
+            return JsonResponse({"success": True, "html": html})
+        return super().get(request, *args, **kwargs)
+
+    def form_invalid(self, form):
+        if self.request.headers.get("x-requested-with") == "XMLHttpRequest":
+            return JsonResponse({"errors": form.errors})
+        return super().form_invalid(form)
+
+    def form_valid(self, form):
+        form.save()
+        if self.request.headers.get("x-requested-with") == "XMLHttpRequest":
+            return JsonResponse({"success": True})
+        return super().form_valid(form)
+
     def handle_no_permission(self):
         if self.request.user.is_authenticated:
             return render(self.request, 'access_denied.html', status=403)
         return super().handle_no_permission()
 
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.groups.filter(name='Administrators').exists():
-            self.form_class = ProductAdminForm
-        # elif request.user.groups.filter(name='Agents').exists():
-        #     self.form_class = UpdatedProductForm
-        # else:
-        #     raise PermissionDenied("You do not have permission to perform this action.")
 
-        return super().dispatch(request, *args, **kwargs)
+# ------------------------- DETAIL -------------------------
+class ProductDetailView(LoginRequiredMixin, DetailView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'product/product_detail.html'
+    permission_required = 'inventory_management.view_product'
+
+    def render_to_response(self, context, **response_kwargs):
+        if self.request.headers.get("x-requested-with") == "XMLHttpRequest":
+            html = render_to_string(self.template_name, context, request=self.request)
+            return JsonResponse({"html": html})
+        return super().render_to_response(context, **response_kwargs)
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        if request.headers.get("x-requested-with") == "XMLHttpRequest":
+            return render(request, "product/product_detail.html", context)
+        return super().get(request, *args, **kwargs)
+
+
+# ------------------------- DELETE -------------------------
+class ProductDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+    model = Product
+    template_name = 'product/product_confirm_delete.html'
+    success_url = reverse_lazy('product_list')
+    permission_required = 'inventory_management.delete_product'
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.delete()
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            return JsonResponse({'success': True})
+        return super().delete(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            obj = self.get_object()
+            html = render_to_string(self.template_name, {'product': obj, 'action_url': self.request.path},
+                                    request=request)
+            return JsonResponse({'success': True, 'html': html})
+        return super().get(request, *args, **kwargs)
+    def handle_no_permission(self):
+        if self.request.user.is_authenticated:
+            return render(self.request, 'access_denied.html', status=403)
+        return super().handle_no_permission()
